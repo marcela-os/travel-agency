b75fa6f3ec617b1318a9137b0ccad40f4e9131d1
Marcelina <m.oscislowska@gmail.com>
2020-09-26 20:07:41 +0200

    Changed OrderOption component

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -1,19 +1,34 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
+import OrderOptionDropdown from './OrderOptionDropdown.js';
+import OrderOptionIcons from './OrderOptionIcons.js';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes.js';
+import OrderOptionNumber from './OrderOptionNumber.js';
 
 
-class OrderOption extends React.Component {
-  render(){
+const optionTypes = {
+  dropdown: OrderOptionDropdown,
+  icons: OrderOptionIcons,
+  checkboxes: OrderOptionCheckboxes,
+  number: OrderOptionNumber,
+};
+
+
+const OrderOption = ({name, type, ...otherProps}) => {
+  const OptionComponent = optionTypes[type];						// Wartością stałej OptionComponent będzie jeden z komponentów z obiektu optionTypes.
+  if(!OptionComponent){
+    return null;
+  } else {
     return (
       <div className={styles.component}>
-        <h3 className={styles.title}> Car Rental </h3>
+        <h3 className={styles.title}>{name}</h3>
+        <OptionComponent
+          {...otherProps}						//Wykorzystujemy go w kodzie JSX i przekazujemy mu wszystkie propsy otrzymane przez OrderOption, poza name i type.
+        />
       </div>
     );
   }
-}
+};
 
-//OrderOption.propTypes = {
-//};
 
 export default OrderOption;
